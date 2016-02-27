cfaddbb72adcd95a9183427bf34e927b03e1aeb3
Eduardo Aaron Fernandez Orallo <aaron_cng@hotmail.com>
2016-02-27 16:22:56 +0100

    Se añaden comentarios

--- BBB_PIC_CAN.X/main.c
+++ BBB_PIC_CAN.X/main.c
@@ -1,8 +1,9 @@
 /* 
  * File:   main.c
- * Author: Edu
- *
+ * Author: Eduardo Aarón Fernández Orallo
  * Created on 23 de febrero de 2016, 22:44
+ * *************************************************
+ * - This programama is a simple example of sending data via CAN bus.
  */
 
 #include <18F25K80.h>
@@ -16,6 +17,7 @@
 
 #use delay(internal=64000000)
 
+//Configure CAN bus speed for 1Mbs
 #define CAN_USE_EXTENDED_ID FALSE
 #define CAN_BRG_SEG_2_PHASE_TS TRUE  
 #define CAN_BRG_PRESCALAR 1 
@@ -36,38 +38,36 @@
 #define LED2_LOW  output_low(PIN_LED2)
 #define LED2_HIGH output_high(PIN_LED2)
 
-#define ASK_FOR_ID_AD_B      0x201  //ask for AD info from CAN port B
+#define WRITE_REGISTER_D_ID      0x100  //CAN bus ID.
 
-//Variable bus CAN
-int32 rx_id;
-int rx_len,i,b;
 struct rx_stat stat;
-unsigned int receive[2];
-unsigned int send[2];
-unsigned int16 control;
-
-
+unsigned int buf[2];
 
 void main ( )
 {
-    i = 88;
-    b = 200;
-    send[0] = 0x24;
-    send[1] = 0x14;
+
+    //Send data.
+    buf[0] = 0x24;
+    buf[1] = 0x14;
     
+    can_init(); // Initialize the can
     
-    can_init(); // always initialize the can
     while(1) {
-       // if ( can_tbe() ) {
-            LED2_HIGH;
-            LED1_LOW;
-            //can_putd(ASK_FOR_ID_AD_B, &i, 1,1,0,0); //put data on transmit buffer
-            can_putd(0x100,&send,2,1,FALSE,FALSE);
-            delay_ms(500);
-            LED1_HIGH;
-            LED2_LOW;
-            delay_ms(500);
-      //  }
+        LED2_HIGH;
+        LED1_LOW;
+        can_putd(WRITE_REGISTER_D_ID,&buf,sizeof(buf),1,FALSE,FALSE); 
+        /*  - Identifier: 0x100
+            - Data pointer: buf
+            - Number of data bytes: sizeof(buf) = 2
+            - Priority (0-3) determines the order messages are sent
+            - Flag to indicate 29 bit identifier
+            - Flag to indicate if this is a data frame
+    (       FALSE) or request for frame (TRUE)
+        */
+        delay_ms(500);
+        LED1_HIGH;
+        LED2_LOW;
+        delay_ms(500);
     }
 }
 
